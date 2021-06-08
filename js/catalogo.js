892dcce4c486e8a3a10644b68fdc667504720b2b
Wanda Scarpato <wandasscarpato@gmail.com>
2021-06-07 22:28:50 -0300

    carrito listo

--- js/catalogo.js
+++ js/catalogo.js
@@ -100,6 +100,7 @@ const productos = [
     imagen: "./img/extras/extra3.jpeg",
     tipo: "extra",
     stock: 10,
+    precio: 999,
     codigo: 13,
   },
   {
@@ -122,6 +123,7 @@ const productos = [
     nombre: "Dessert Shooters",
     imagen: "./img/extras/extra4.jpeg",
     tipo: "extra",
+    precio: 999,
     stock: 10,
     codigo: 16,
   },
@@ -142,15 +144,82 @@ const productos = [
     codigo: 18,
   }
 ];
+/*Carga de productos*/
+function verDulce() {
+  let tipo = "dulce";
+  mostrarFiltrado(tipo);
+}
+function verSalado() {
+  let tipo = "salado";
+  mostrarFiltrado(tipo);
+}
+function verExtras() {
+  let tipo = "extra";
+  mostrarFiltrado(tipo);
+}
+function verTodo() {
+  mostrarTodo();
+}
+mostrarTodo();
 let contenedor=document.getElementById("contenedor");
 let contadorCarrito=document.getElementById("contadorCarrito");
 let precioFinal=document.getElementById("precioFinal");
 let modal=document.getElementById("modal");
+let cantidadDeProductos=document.querySelector(".sinProductos");
+
+function limpiarProductos() {
+  document.querySelector("#main-app").innerHTML = "";
+}
 
 let carritoDeCompras = JSON.parse(localStorage.getItem('carritoDeCompras') || '[]');
 // cargo los productos
-function cargarProductos() {
-  limpiarPantalla();
+function mostrarTodo() {
+  limpiarProductos();
+  productos.forEach((producto) => {
+    //  CREO ESTRUCTURA DEL PRODUCTO
+    const div = document.createElement(`div`);
+    div.innerHTML = `
+    <div class="productos">
+        <div class="productos-producto">
+            <div>
+                ${producto.nombre}
+            </div>
+            <div>
+            <img class="img-productos" src="
+                ${producto.imagen}">
+            </div>
+            <div>
+                ${producto.precio}
+            </div>
+            <div id="${producto.codigo}cantidadDisponible">
+            </div>
+            <div><button id="${producto.codigo}" onclick="botonCarrito(${producto.codigo})">Agregar al carrito</button>
+            </div>
+        </div>
+    </div>`;
+    // AGREGO EL DIV DEL PRODUCTO A LA APP
+    document.querySelector("#main-app").appendChild(div);
+    // SI YA TENGO UN CARRITO CALCULO EL STOCK MENOS EL CARRITO
+    let stockAjustadoAlCarro = Number(ajustarStock(producto.stock,producto.codigo));
+    // CARGO EL SELECT CON LAS OPCIONES DE STOCK
+    cantidadXStock(stockAjustadoAlCarro,producto.codigo);
+    //AGREGO EVENTO AL BOTON
+    let boton = document.getElementById(`${producto.codigo}`);
+    boton.addEventListener("click", () => {
+      //,e fijo si esta deshabilitado el select
+      if(!(document.getElementById(`${producto.codigo}cantidadDisponibleb`).disabled)){
+        agregarAlCarrito(`${producto.codigo}`);
+      } else {
+        swal("El producto seleccionado no se encuentra disponible", "", "error");
+      }
+    })
+  });
+}
+
+function devolverStock(id) {
+  // LIMPIO TODO
+  limpiarProductos();
+  // NUEVAMENTE CARGO CADA PRODUCTO
   productos.forEach((producto) => {
     const div = document.createElement(`div`);
     div.innerHTML = `
@@ -168,8 +237,7 @@ function cargarProductos() {
             </div>
             <div id="${producto.codigo}cantidadDisponible">
             </div>
-            <div>
-            <button id="${producto.codigo}" onclick="botonCarrito()">Agregar al carrito</button>
+            <div><button id="${producto.codigo}" onclick="botonCarrito(${producto.codigo})">Agregar al carrito</button>
             </div>
         </div>
     </div>`;
@@ -177,23 +245,17 @@ function cargarProductos() {
     cantidadXStock(producto.stock,producto.codigo);
     let boton = document.getElementById(`${producto.codigo}`);
     boton.addEventListener("click", () => {
-      agregarAlCarrito(`${producto.codigo}`);
+      if(!(document.getElementById(`${producto.codigo}cantidadDisponibleb`).disabled)){
+        agregarAlCarrito(`${producto.codigo}`);
+      } else {
+        swal("El producto seleccionado no se encuentra disponible", "", "error");
+      }
     })
   });
 }
 
-function cantidadXStock(stock, id){
- let dispo=document.getElementById(`${id}cantidadDisponible`);
- let opciones;
-  for(let i=0; i<=stock; i++){
-    opciones+=`<option> ${i+1} </option>`
-  }
-  dispo.innerHTML=`<select> ${opciones} </select>`
-}
-
 function mostrarFiltrado(filtro) {
-  limpiarPantalla();
-
+  limpiarProductos();
   productos.forEach((producto) => {
     const div = document.createElement(`div`);
     if (producto.tipo == filtro) {
@@ -213,118 +275,276 @@ function mostrarFiltrado(filtro) {
         <div id="${producto.codigo}cantidadDisponible">
         </div>
         <div>
-        <button id="${producto.codigo}" onclick="botonCarrito()">Agregar al carrito</button>
+        <button id="${producto.codigo}" onclick="botonCarrito(${producto.codigo})">Agregar al carrito</button>
         </div>
             </div>
         </div>`;
       document.querySelector("#main-app").appendChild(div);
-    cantidadXStock(producto.stock,producto.codigo);
+    let stockAjustadoAlCarro = Number(ajustarStock(producto.stock,producto.codigo));
+    cantidadXStock(stockAjustadoAlCarro,producto.codigo);
     let boton = document.getElementById(`${producto.codigo}`);
     boton.addEventListener("click", () => {
-      agregarAlCarrito(`${producto.codigo}`);
-      });
+      if(!(document.getElementById(`${producto.codigo}cantidadDisponibleb`).disabled)){
+        agregarAlCarrito(`${producto.codigo}`);
+      } else {
+        swal("El producto seleccionado no se encuentra disponible", "", "error");
+      }
       
+    })
     }
   });
 }
-/*CARRITO---------------------------------*/
 
-function inicializarCarrito() {
-  const items = carritoDeCompras.filter(articulo => articulo);
-  items.forEach(articulo => {
-    agregarItemAlCarrito(articulo);
-    contadorCarrito.innerHTML= items.length;
-  });
-} 
+function ajustarStock(stock, id){
+  // INTENTO VER SI HAY ALGO EN EL CARRO, SI HAY BUSCO EL ID QUE LE PASO
+  // Y AL STOCK TOTAL DEL PRODUCTO DEL ARRAY LE SACO LO QUE TENGO EN EL
+  // CARRITO Y DEVUELVO EL STOCK PARA LA PAGINA
+  try{
+    //cargo en la constante el carrito
+    const productosEnCarro =  JSON.parse(localStorage.carritoDeCompras);
+
+    productosEnCarro.forEach(articulo => {
+
+        if(articulo.codigo == id){
+
+          productos.forEach(producto => {
+
+            if(producto.codigo == id){
+
+              producto.stock = producto.stock - articulo.stock;
+              stock = producto.stock;
+
+            }
+        });
+      }
+    });
+    return stock;
+  }
+  catch{
+    return stock;
+  }
+}
+
+function cantidadXStock(stock, id){
+  // OBTENGO EL DIV PARA EL SELECT DEL PRODUCTO
+  let dispo=document.getElementById(`${id}cantidadDisponible`);
+  let opciones;
+  //Me fijo el stock disponible
+  if(stock != 0){
+    // POR CADA NUMERO DEL STOCK AGREGO UN OPTION
+    for(let i=0; i<stock; i++){
+      opciones+=`<option  value="${i+1}"> ${i+1} </option>`
+    }
+    // CARGO EL SELECT CON LOS OPTION AL DIV
+    dispo.innerHTML=`<select id="${id}cantidadDisponibleb" > ${opciones} </select>`
+  }else{
+    opciones+=`<option  value="${0}"> ${0} </option>`
+    dispo.innerHTML=`<select id="${id}cantidadDisponibleb" disabled> ${opciones} </select>`
+  }
+}
+
+function TomarSeleccionado(cod){
+  var combo = document.getElementById(`${cod}cantidadDisponibleb`);
+  combo.options[combo.selectedIndex].text;
+  cartelCarrito(); 
+}
+
+function botonCarrito(cod) {
+ TomarSeleccionado(cod);
+ swal("Agregado al carrito", "", "success");
+}
+/*CARRITO---------------------------------*/
 inicializarCarrito();
 
-function verCarrito(){
-  modal.style.display="block";
- document.querySelector("body").style.overflow="hidden";
- sinProductos();
+function inicializarCarrito() {
+  // ABRO EL CARRITO Y REVISO LO QUE TENGA CARGADO
+  let contador=0;
+  const items = carritoDeCompras.filter(articulo => articulo);
+  let tipo = "inicio";
+  items.forEach(articulo => {
 
+    agregarItemAlCarrito(articulo,tipo);
+    // PONGO NUMERO AL LADO DEL CARRO
+      contador+=Number(articulo.stock);
+    cartelCarrito();
+  });
+  contadorCarrito.innerText = contador;
+} 
+
+function verCarrito(){
+ modal.style.display="block";
+ cartelCarrito()
 }
-function sinProductos(){
+function cartelCarrito(){
   if(carritoDeCompras.length==0){
-    document.querySelector(".sinProductos").style.display="block";
+    cantidadDeProductos.style.display="block";
+    cantidadDeProductos.style.top="120px"
     document.querySelector(".precio").style.display="none";
   }else{
-    document.querySelector(".sinProductos").style.display="none";
+    cantidadDeProductos.style.top="200px"
+    cantidadDeProductos.style.display="none";
     document.querySelector(".precio").style.display="block";
   } 
 }
 function closeModal(){
   modal.style.display="none";
-  document.querySelector("body").style.overflow="auto";
 }
 function agregarAlCarrito(id) {
-    let validar = carritoDeCompras.some(x=> x.codigo == id);
-    if(validar){
-        contadorCarrito.innerHTML=carritoDeCompras.filter(articulo => articulo).length++;
-    }else{
- //Agregar producto al carrito
-  let productoAgregar = productos.filter(elemento => elemento.codigo == id)[0];
-  carritoDeCompras.push(productoAgregar);
-  agregarItemAlCarrito(productoAgregar);
-  actualizarCarrito();
-  //eliminarProductoDelCarrito(productoAgregar);
-}
+  // ME FIJO SI EL CARRO YA TIENE UN PRODUCTO CON EL ID QUE LE PASO
+  let validar = carritoDeCompras.some(x=> x.codigo == id);
+
+  if(validar){
+    // SI YA TIENE BUSCO EL ARTICULO Y LE SUMO STOCK
+    const items = carritoDeCompras.filter(articulo => articulo);
+    items.forEach(articulo => {
+      if(articulo.codigo == id){
+        // ME FIJO LO QUE SELECCIONO
+        let unidades=cantidadSeleccionada(articulo);
+        // SE LO SUMO A LO QUE YA TENIA
+        let stock = articulo.stock+Number(unidades);
+        articulo.stock = Number(stock);
+        carritoDeCompras = items;
+        // AJUSTO EL STOCK DEL SELECT
+        // VOY A TENER QUE BUSCAR EL STOCK ACTUAL Y SACARLE EL CARRO
+        productos.forEach((producto) => {
+          if(producto.codigo==id){
+            producto.stock  = Number(producto.stock-unidades);
+            cantidadXStock(producto.stock,id);
+          }
+        });
+        actualizarItemCarrito(id);
+        actualizarCarrito();
+      }
+    });
+    //contadorCarrito.innerHTML=carritoDeCompras.filter(articulo => articulo).length++;
+  }else{
+    //Agregar producto al carrito
+    let productoAgregar = productos.filter(elemento => elemento.codigo == id)[0];
+    let unidades=cantidadSeleccionada(productoAgregar);
+    const productoParaCarrito =
+      {
+        nombre: productoAgregar.nombre,
+        imagen: productoAgregar.imagen,
+        tipo: productoAgregar.tipo,
+        precio: productoAgregar.precio,
+        stock: productoAgregar.stock,
+        codigo: productoAgregar.codigo,
+      };
+    let tipo = "nuevo";
+    agregarItemAlCarrito(productoAgregar, tipo);
+    let stockTotal = productoAgregar.stock;
+    productoParaCarrito.stock = Number(unidades);
+    carritoDeCompras.push(productoParaCarrito);
+    actualizarCarrito();
+    //productoParaCarrito.stock = Number(stockTotal);
+    //eliminarProductoDelCarrito(productoAgregar);
+  }
 }
 
-function eliminarProductoDelCarrito(productoAgregar) {
-  let botoneliminar=document.getElementById(`${productoAgregar}b`)
-  //botoneliminar.addEventListener(`click`, () => {
-    botoneliminar.parentElement.remove();
-    console.log(botoneliminar.parentElement);
-    carritoDeCompras = carritoDeCompras.filter((elemento) => elemento.codigo != productoAgregar);
+function actualizarItemCarrito(id) {
+
+  let botoneliminar=document.getElementById(`${id}b`)
+  botoneliminar.parentElement.remove();
+
+  const items = carritoDeCompras.filter(articulo => articulo);
+  items.forEach(articulo => {
+    if(articulo.codigo == id){
+      let div = document.createElement(`div`);
+      div.classList.add("contenedor-carrito-productos");
+      div.innerHTML += `
+            <p>${articulo.nombre} </p>
+            <p>Precio: $${articulo.precio}  </p>
+            <p>Unidades:${articulo.stock}</p>
+            <button id="${id}b" onclick="eliminarYRefrescar(${id})" class="eliminar-producto">Eliminar</button>
+            `;
+      contenedor.appendChild(div);
+    }
+  });
+  // ACTUALIZO EL CARRO PARA REFLEJAR LOS CAMBIOS
+  actualizarCarrito();
+}
+
+function eliminarProductoDelCarrito(id) {
+  // OBTENGO LOS PRODUCTOS DEL CARRO
+  const productosEnCarro =  JSON.parse(localStorage.carritoDeCompras);
+  productosEnCarro.forEach(productoEnCarro => {
+    // CUANDO EL ID  COINCIDE CON LO QUE TENGO EN EL CARRO
+      if(productoEnCarro.codigo == id){
+        productos.forEach(producto => {
+          // SUMO AL STOCK LO DEL CARRO
+          if(producto.codigo == id){
+            producto.stock = productoEnCarro.stock + producto.stock;
+          }
+      });
+    }
+  });
+  // ELIMINO DEL CARRO LO VISUAL
+  let botoneliminar=document.getElementById(`${id}b`)
+  botoneliminar.parentElement.remove();
+  // GUARDO EL CARRO DE NUEVO CON TODO EXCEPTO EL ID QUE SAQUE RECIEN
+  carritoDeCompras = carritoDeCompras.filter((elemento) => elemento.codigo != id);
+  devolverStock(id);
   //});
 }
 function eliminarYRefrescar(id){
   eliminarProductoDelCarrito(id);
   actualizarCarrito();
 }
-function agregarItemAlCarrito(productoAgregar) {
+function agregarItemAlCarrito(productoAgregar, tipo) {
   if (!productoAgregar) return;
-  let cantidadProd=document.getElementById(`${productoAgregar.codigo}b`);
+  // CREO UN DIV PARA METER EL ITEM DEL CARRITO
   let div = document.createElement(`div`);
+
+  if(tipo=="nuevo"){
+    // ESTOY AGREGANDO UN PRODUCTO NUEVO ENTONCES TENGO QUE AJUSTAR EL STOCK
+    // UNIDADES TIENE EL NUMERO DEL SELECT
+    var unidades=cantidadSeleccionada(productoAgregar);
+    // LE SACO DEL STOCK LO QUE ESTA EN EL CARRITO
+    disminuirStock(productoAgregar, unidades);
+  } else if (tipo=="inicio"){
+    // SI RECARGO LA PAGINA NO TENGO QUE RESTAR STOCK PORQUE YA LO HICE CUANDO INICIO
+    var unidades=productoAgregar.stock;
+  }
+  // AGREGO CLASES Y DATOS AL ITEM DEL CARRITO
   div.classList.add("contenedor-carrito-productos");
   div.innerHTML += `
-        <p>Producto: ${productoAgregar.nombre} </p>
-        <p>Precio: ${productoAgregar.precio}  </p>
-        <p>${cantidadProd}</p>
-        <button id="${productoAgregar.codigo}b" onclick="eliminarYRefrescar(${productoAgregar.codigo})" class="eliminar-producto">eliminar</button>
+        <p>${productoAgregar.nombre} </p>
+        <p>Precio: $${productoAgregar.precio}  </p>
+        <p>Unidades:${unidades}</p>
+        <button id="${productoAgregar.codigo}b" onclick="eliminarYRefrescar(${productoAgregar.codigo})" class="eliminar-producto">Eliminar</button>
         `;
   contenedor.appendChild(div);
+  // ACTUALIZO EL CARRO PARA REFLEJAR LOS CAMBIOS
+  actualizarCarrito();
 }
-
+function disminuirStock(productoAgregar, unidades){
+  // RECORRO EL ARRAY BUSCO EL PRODUCTO Y LE RESTO LAS UNIDADES QUE AGREGUE AL CARRITO RECIEN
+  var nuevoStock;
+  productos.forEach(producto => {
+    if(producto.codigo == productoAgregar.codigo){
+      producto.stock = producto.stock - unidades;
+      nuevoStock = producto.stock;
+    }
+  });
+  // UNA VEZ QUE RESTE EL STOCK CARGO DE NUEVO EL SELECT
+  cantidadXStock(nuevoStock, productoAgregar.codigo)
+}
+function cantidadSeleccionada(productoAgregar){
+  var combo = document.getElementById(`${productoAgregar.codigo}cantidadDisponibleb`);
+  var unidades=combo.options[combo.selectedIndex].text;
+  return unidades;
+}
 function actualizarCarrito(){
-    contadorCarrito.innerText = carritoDeCompras.length;
+  let preciof=0;
+let contador=0;
     localStorage.setItem("carritoDeCompras", JSON.stringify(carritoDeCompras));
-    precioFinal.innerHTML = carritoDeCompras.reduce((acc, el)=> acc + el.precio, 0);
-    sinProductos();
-}
-function limpiarPantalla() {
-  document.querySelector("#main-app").innerHTML = "";
-}
-function botonCarrito() {
- swal("Agregado al carrito", "", "success");
+   // precioFinal.innerHTML = carritoDeCompras.reduce((acc, el)=> acc + el.precio, 0);
+    carritoDeCompras.forEach(producto=>{
+      preciof+=Number(producto.precio)*Number(producto.stock);
+      contador+=Number(producto.stock);
+    })
+    precioFinal.innerHTML =preciof;
+    contadorCarrito.innerText = contador;
+    cartelCarrito();
 }
 
-const contenedorCarrito = document.getElementById("contenedorCarrito");
-
-function verDulce() {
-  let tipo = "dulce";
-  mostrarFiltrado(tipo);
-}
-function verSalado() {
-  let tipo = "salado";
-  mostrarFiltrado(tipo);
-}
-function verExtras() {
-  let tipo = "extra";
-  mostrarFiltrado(tipo);
-}
-function verTodo() {
-  cargarProductos();
-}
-cargarProductos();
